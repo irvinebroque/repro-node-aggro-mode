b60546c32bc13983420e5dcaa4f510d099bb98d3
Brendan Irvine-Broque <birvine-broque@cloudflare.com>
2023-09-27 19:30:51 -0700

    Highlight how warning is overreaching

--- src/foo.ts
+++ src/foo.ts
@@ -1,7 +1,17 @@
 export async function foo() {
     // Only try to import node:net if not in a Workers environment
     if (navigator.userAgent !== 'Cloudflare-Workers') {
-        const net = await import('node:net'); 
+        // Esbuild sees this, and we warn you:
+        //     ✘ [ERROR] Could not resolve "node:net"
+        //     src/foo.ts:4:28:
+        //       4 │         const net = require('node:net');
+        //         ╵                             ~~~~~~~~~~
+        //   The package "node:net" wasn't found on the file system but is built into node.
+        //   Add "node_compat = true" to your wrangler.toml file to enable Node.js compatibility.
+
+        // Even though this line is never actually reached:
+
+        const net = await import('node:net'); // Or const net = require('node:net');
     }
     // else...
 }
